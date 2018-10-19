a880bed41f7faf1e4483e81e70438504676be07d
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2018-10-19 15:36:29 +0530

    find unique elements of an array

---
+++ findUniques/libTest.js
@@ -0,0 +1,32 @@
+let assertEq = require('assert').deepEqual;
+let {findUniques}  = require('./lib.js');
+
+const testfindUniques = function(input,expected,func){
+  let actual = func(input);
+  console.log("For input :", input);
+  console.log("expected :", expected);
+  console.log("actual :", actual);
+  console.log("------------");
+  assertEq(expected, actual);
+}
+
+//test with empty array :
+input = [];
+testfindUniques(input,[],findUniques);
+
+//test with one number :
+input = [1];
+testfindUniques(input,[1],findUniques);
+
+//test with two same numbers :
+input = [1,1];
+testfindUniques(input,[1],findUniques);
+
+//test with 3 same numbers 
+input = [-1,-1,-1];
+testfindUniques(input,[-1],findUniques);
+
+//test with multiple numbers :
+input = [1,2,3,-1,2,3,0,-0,12,3];
+testfindUniques(input,[1,2,3,-1,0,12],findUniques);
+
---
+++ findUniques/sourceFile.js
@@ -0,0 +1,4 @@
+const {findUniques} = require('./lib.js');
+const input = [1,2,3,4,5,6,12,1,2,3,4,0,-0];
+console.log('for input:', input);
+console.log('unique elements are:', findUniques(input));
