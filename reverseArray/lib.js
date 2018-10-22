f2cbe6b92f40d0f05735016a46a885acd741e39d
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2018-10-22 23:14:08 +0530

    reverses the element of an array

--- reverseArray/lib.js
+++ reverseArray/lib.js
@@ -1,11 +1,5 @@
 const reverseArray  = function(list){
- let result = [];
-
-  for(let index = list.length-1; index >= 0; index --){
-    result.push(list[index]);
-  }
-
-  return result;
+  return list.reverse();
 }
 
 exports.reverseArray = reverseArray;
