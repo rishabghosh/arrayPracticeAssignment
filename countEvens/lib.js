c243dd7ef2e5baed7674daa77215b259b1a72d6e
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2018-10-22 22:16:52 +0530

    count even numbers in a array
    now uses atomic functions
    also uses inbuilt function to reduce complexity.

--- countEvens/lib.js
+++ countEvens/lib.js
@@ -1,11 +1,18 @@
-const countEvens = function(list){
-  const result = [];
-  for(let index = 0; index < list.length; index ++){
-    if(list[index] %2 === 0){
-      result.push(list[index])
-    }
+const isEven = function(counter, number){
+  let {count} = counter;
+  if( number %2 === 0){
+    count ++;
   }
-  return result;
+  return count;
+}
+ 
+const countEvens = function(list){
+  let counter = {count : 0};
+  for(let index = 0; index < list.length; index ++){
+    counter.count = isEven(counter, list[index]);
+
+ }
+  return counter.count;
 }
 
 exports.countEvens = countEvens;
--- countEvens/libTest.js
+++ countEvens/libTest.js
@@ -3,36 +3,37 @@ let {countEvens}  = require('./lib.js');
 
 const testCountEvens = function(input,expected,func){
   let actual = func(input);
-  assertEq(expected, actual);
-  console.log("For input :", input);
+    console.log("For input :", input);
   console.log("expected :", expected);
   console.log("actual :", actual);
   console.log("------------");
+  assertEq(expected, actual);
+
 }
 //test with zero :
 input = [0];
-testCountEvens(input,[0],countEvens);
+testCountEvens(input, 1, countEvens);
 
 //test with one odd :
 input = [1];
-testCountEvens(input,[],countEvens);
+testCountEvens(input, 0, countEvens);
 
 //test with one even :
 input = [2];
-testCountEvens(input,[2],countEvens);
+testCountEvens(input, 1, countEvens);
 
 //test with negetive odd :
 input = [-1];
-testCountEvens(input, [], countEvens);
+testCountEvens(input, 0, countEvens);
 
 //test with negetive even :
 input = [-2];
-testCountEvens(input,[-2],countEvens);
+testCountEvens(input, 1, countEvens);
 
 //test with two evens :
 input = [-2, 2];
-testCountEvens( input, [-2, 2], countEvens);
+testCountEvens( input, 2, countEvens);
  
 //test with multiple numbers : 
 input = [1,2,3,4,5,6,0,-1,-2,-3,-4,-5,-6]
-testCountEvens( input,[2,4,6,0,-2,-4,-6], countEvens);
+testCountEvens( input, 7, countEvens);
