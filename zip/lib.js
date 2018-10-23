d0e78da86e93cc30c21b1390926ddb4b99fa00ad
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2018-10-23 23:22:55 +0530

    insert elements from two arrays in a result array

--- zip/lib.js
+++ zip/lib.js
@@ -1,14 +1,10 @@
 const zipElements = function(list1, list2){
   let result = [];
-  let maxLength = list1.length;
-  if(list2.length < maxLength){
-    maxLength = list2.length;
-  }
+  let leastLength = Math.min(list1.length, list2.length);
 
-  for(let index = 0; index < maxLength; index ++){
+  for(let index = 0; index < leastLength; index ++){
     result.push([ list1[index], list2[index] ]);
   }
   return result;
 }
-
 exports.zipElements = zipElements;
